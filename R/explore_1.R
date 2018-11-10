519e67fb800c3c16dde0e67822b9f797c8d47f29
datawranglerai <james.wolman@datawrangler.ai>
2018-11-10 12:10:45 +0000

    [ADD] - Survey schema formatting and collation of MCQ data

---
+++ R/explore_1.R
@@ -0,0 +1,56 @@
+library(readr)
+library(dplyr)
+library(stringr)
+
+format_survey_schema <- function(x) {
+        #'
+        #' Formats the survey schema file for the 2018 Kaggle ML & DS Survey
+        #' Challenge into a tidy, analysis-friendly format.
+        #'
+        #' @param x Character. Path to Kaggle survey schema file.
+        #'
+        #' @return Dataframe. Number of respondents per question as well as
+        #' question text, sorted by question number.
+        #'
+        #' @references 
+        #' Kaggle competition overview: https://www.kaggle.com/kaggle/kaggle-survey-2018/home
+        #' Schema details: https://www.kaggle.com/kaggle/kaggle-survey-2018#SurveySchema.csv
+        #'
+
+        survey_schema_raw <- read_csv(x) %>%
+                select(starts_with("Q"))       # select question columns only
+        
+        survey_schema <- data_frame(
+                question_label  = colnames(survey_schema_raw),
+                question_text   = as.character(survey_schema_raw[1,]),
+                respondents     = as.numeric(survey_schema_raw[2,]),
+                question_number = as.numeric(sub("Q", "", question_label))
+        ) %>%
+                arrange(question_number)
+        
+        survey_schema
+        
+}
+
+format_mcq_schema <- function(x) {
+        #'
+        #' 
+        #' 
+        
+        survey_schema <- format_survey_schema("./DATA/kaggle-survey-2018/SurveySchema.csv")
+        
+        mcq_schema_raw <- read_csv(x, n_max = 1) %>%
+                select(starts_with("Q")) 
+        
+        mcq_schema <- data_frame(
+                question_label_detailed  = colnames(mcq_schema_raw),
+                question_text_detailed   = as.character(mcq_schema_raw[1,]),
+                question_label           = str_match(question_label_detailed, "(^Q\\d+).*")[,2]
+        ) %>%
+                filter(!grepl("TEXT", question_label_detailed)) %>%
+                left_join(survey_schema, by = "question_label") %>%
+                mutate(question_text_detailed = str_match(question_text_detailed, ".*Selected Choice - (.*)$")[,2]) 
+                
+        mcq_schema
+        
+}
---
+++ R/multiple_choice_summary.R
@@ -0,0 +1,35 @@
+# Import schema
+source("./R/import_schema.R")
+survey_schema <- format_survey_schema("./DATA/kaggle-survey-2018/SurveySchema.csv")
+
+multiple_choice_responses <- read_csv("./DATA/kaggle-survey-2018/multipleChoiceResponses.csv")
+
+# Remove any columns with user-entered text
+cols <- colSums(mapply(grepl, ".* - Text$", multiple_choice_responses))
+responses_mcq <- multiple_choice_responses[, which(cols == 0)]
+
+## Tally up results for each question
+mcq_data <- vector("list", length = nrow(survey_schema))
+
+for ( i in 1:nrow(survey_schema) ) {
+        
+        question  <- survey_schema$question_label[i]
+        col_regex <- paste0("^", question, "(?:\\D.*)?$")
+        
+        cat("Processing question", question, "\n")
+        
+        # Summarize MCQ results for each question
+        mcq_data[[i]] <- responses_mcq %>%
+                filter(row_number() > 1) %>%  # remove question text row   
+                select(matches(col_regex)) %>%
+                gather(question, response,
+                       matches(question),
+                       na.rm = TRUE) %>%
+                group_by(response) %>%
+                summarize(count = n())
+        
+}
+
+names(mcq_data) <- paste(survey_schema$question_label, 
+                         survey_schema$question_text,
+                         sep = ": ")
