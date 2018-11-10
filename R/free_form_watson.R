395e6bdaadf9d1375da4c480ee9dcbd6577e7862
datawranglerai <james.wolman@datawrangler.ai>
2018-11-10 18:58:50 +0000

    [ADD] - Turned Watson query into function and added NLU unit cost calculation

--- R/free_form_watson.R
+++ R/free_form_watson.R
@@ -4,6 +4,7 @@
 
 # NLU docs: https://console.bluemix.net/apidocs/natural-language-understanding#related-information
 
+
 library(readr)
 library(dplyr)
 library(httr)
@@ -20,31 +21,95 @@ sort(sapply(freeforms, function(x) length(x[!is.na(x)])), decreasing = TRUE) / n
 # Personality insights
 # Tone analysis
 
-# IBM Watson API anatomy
-API_KEY     <- "PdW3hpeF7RmNOqWoJZwZsm7TtY3nOLZKB96PX3UX4Gxr"
-SERVICE_URL <- "https://gateway-fra.watsonplatform.net/natural-language-understanding/api"
-METHOD      <- "/v1/analyze"
-VERSION     <- "2018-03-16"
+watson_calculate_nlu_units <- function(text, features) {
+        #'
+        #' Calculates the number of NLU units that will be used up in an API
+        #' request.
+        #' 
+        #' @param characters. Character. Text to be analysed.
+        #' @param features. Named list. Features being requested.
+        #'
+        #' @return Numeric. Number of NLU units to be used.
+        #'
+        #' @note 
+        #' NOTE: A NLU item is based on the number of data units enriched and
+        #' the number of enrichment features applied. A data unit is 10,000
+        #' characters or less. For example: extracting Entities and Sentiment
+        #' from 15,000 characters of text is (2 Data Units * 2
+        #' Enrichment Features) = 4 NLU Items. A custom model refers to an
+        #' annotation model developed with Watson Knowledge Studio.
+        #' 
+        #' @references 
+        #' NLU API: https://www.ibm.com/watson/services/natural-language-understanding/
+        #'
+        
+        data_units <- ceiling(nchar(text) / 10000)
+        enrichment_features <- length(parameters[["features"]])
+        
+        data_units * enrichment_features
+}
+
+watson_NLU_query <- function(text, parameters, api_key) {
+        #'
+        #' Passes text through IBM Watson's Natural Language Understanding API.
+        #'
+        #' @param text Character. Text to be analysed by the
+        #' @param parameters Named list. Parameters to be passed to the body of
+        #' the API request.
+        #'
+        #' @return Dataframe. Entities identified in the text.
+        #'
+        #' @references 
+        #' API documentation: https://console.bluemix.net/apidocs/natural-language-understanding#related-information
+        #'
+        
+        library(httr)
+        library(jsonlite)
+        
+        # Service anatomy
+        SERVICE_URL <- "https://gateway-fra.watsonplatform.net/natural-language-understanding/api"
+        METHOD      <- "/v1/analyze"
+        VERSION     <- "2018-03-16"
+        
+        parameters[["text"]] <- text
+        
+        cat("This request will use", watson_calculate_nlu_units(text, parameters), "NLU units\n")
+        
+        # Make API call
+        res <- POST(
+                url = paste0(SERVICE_URL, METHOD),
+                body = parameters,
+                encode = "json",
+                query = list(version = VERSION),
+                add_headers("Content-Type" = "application/json"),
+                authenticate("apikey", api_key)
+        )
+        
+        api_response <- content(res, "text")
+        api_response_parsed <- fromJSON(api_response, flatten = TRUE, simplifyDataFrame = TRUE)
+        
+        api_response_parsed$entities
+        
+}
+
+API_KEY <- "PdW3hpeF7RmNOqWoJZwZsm7TtY3nOLZKB96PX3UX4Gxr"
+json <- fromJSON("./JSON/watson_body_entities.json") # parameters for entity request
+
+# QUESTION 38 ----
 
 # Entity analysis of Q38 to find top media sources
 q38_responses <- freeforms$Q38_OTHER_TEXT[-1] %>% na.omit()
+text <- paste(q38_responses, collapse = ", ")
 
-json <- fromJSON("./JSON/watson_body_entities.json")
 
-json$text <- paste(q38_responses, collapse = ", ")
 
-# Make API call
-res <- POST(
-        url = paste0(SERVICE_URL, METHOD),
-        body = json, encode = "json",
-        query = list(version = VERSION),
-        add_headers("Content-Type" = "application/json"),
-        authenticate("apikey", API_KEY),
-        verbose()
-        )
+# Find entities
+entities_q38 <- watson_NLU_query(text, parameters = json, api_key = API_KEY)
 
-res
-api_response <- content(res, "text")
-api_response_parsed <- fromJSON(api_response, flatten = TRUE, simplifyDataFrame = TRUE)
 
-entities <- api_response_parsed$entities
+# QUESTION 12 ----
+
+q12_responses <- freeforms$Q12_OTHER_TEXT[-1] %>% na.omit()
+text <- paste(q12_responses, collapse = ", ")
+
+entities_q12 <- watson_NLU_query(text, json, API_KEY)
